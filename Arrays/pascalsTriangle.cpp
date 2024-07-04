6648e6250f22bb4f6afc1ca19d0b5bab64640c54
Kushala01 <wjsjkushala9@gmail.com>
2024-07-05 00:41:44 +0400

    july-4th

---
+++ Arrays/pascalsTriangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> dp;
+
+        for(int i = 0; i < numRows; i++) {
+            vector<int> pq;
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) {
+                    pq.push_back(1);
+                } else {
+                    int ele = dp[i - 1][j] + dp[i - 1][j - 1];
+                    pq.push_back(ele);
+                }
+            }
+            dp.push_back(pq);
+        }
+        
+        return dp;
+    }
+};
