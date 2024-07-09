3f1a94920d8d9d24e5e8ae9f219a74fd19e810b1
Kushala01 <wjsjkushala9@gmail.com>
2024-07-09 20:02:55 +0400

    july-9th

--- Arrays/findMissingNrepeatingNumbers/brute.cpp
+++ Arrays/findMissingNrepeatingNumbers/brute.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 
 vector<int> findMissingRepeatingNumbers(vector<int> a) {
-    int n = a.size(); // size of the array
-    int hash[n + 1] = {0}; // hash array
-
-    //update the hash array:
-    for (int i = 0; i < n; i++) {
-        hash[a[i]]++;
-    }
-
-    //Find the repeating and missing number:
+    int n = a.size(); 
     int repeating = -1, missing = -1;
     for (int i = 1; i <= n; i++) {
-        if (hash[i] == 2) repeating = i;
-        else if (hash[i] == 0) missing = i;
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[j] == i) cnt++;
+        }
+
+        if (cnt == 2) repeating = i;
+        else if (cnt == 0) missing = i;
 
         if (repeating != -1 && missing != -1)
             break;
@@ -30,3 +27,5 @@ int main()
          << ans[0] << ", " << ans[1] << "}\n";
     return 0;
 }
+//Time Complexity: O(N2)
+//Space Complexity: O(1)
